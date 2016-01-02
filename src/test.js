29d704dd37558a9c9ad395709bf56009a383e300
陈样 <chen_yang@zju.edu.cn>
2016-01-02 14:05:41 +0800

    Interpreter

---
+++ src/test.js
@@ -0,0 +1,32 @@
+var a = 0 ;
+var b = 0 ;
+
+function test()
+{
+	a = 1.0 ;
+	return a ;
+}
+
+if ( a > 0 ) {
+	b = 1 ;	
+}
+else if (b>0) {
+	a = 1;
+}
+
+var array = new Array() ;
+
+var n = 10 ;
+
+for ( var i = 0 ; i < n ; i = i+1 )
+{
+	array = i ;
+}
+
+while (n) {
+	print array[i] + " ";
+}
+
+test(1,2);
+
+a = b;
