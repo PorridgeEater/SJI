c0f67de8a8afd794e18f3047a1e3135d08010005
PorridgeEater <fanhr5@gmail.com>
2016-01-06 04:12:33 +0800

    finish all variable test
    finish all variable test cases

--- test/variable/real_str_logic.js
+++ test/variable/real_str_logic.js
@@ -7,23 +7,28 @@ var strF = "5";
 
 console.log("TEST STRING == != < >");
 console.log("---------------------------");
-console.log(strA + " != " + strB + " = " + (strA!=strB));
-console.log(strB + " == " + strC + " = " + (strB==strC));
+console.log(strA + " != " + strB + " : " + (strA!=strB));
+console.log(strB + " == " + strC + " : " + (strB==strC));
 
-console.log(strA + " < " + strB + " = " + (strA<strB));
+console.log(strA + " < " + strB + " : " + (strA<strB));
 console.log("");
 
 console.log("COMPARE NUM STRING AND NUM STRING");
 console.log("---------------------------");
-console.log(strE + " < " + strF + " = " + (strE < strF));
+console.log(strE + " < " + strF + " : " + (strE < strF));
 console.log("");
 
-
+var intA = 5;
 console.log("COMPARE NUM STRING AND NUM");
 console.log("---------------------------");
-console.log(strA + " != " + strB + " = " + (strA!=strB));
-console.log(strB + " == " + strC + " = " + (strB==strC));
+console.log(strE + " < " + intA + " : " + (strE < intA));
+console.log("");
 
-console.log(strA + " < " + strB + " = " + (strA<strB));
+console.log("COMPARE STRING AND NUM");
+console.log("---------------------------");
+console.log(strB + " < " + intA + " : " + (strB < intA));
+console.log(strB + " > " + intA + " : " + (strB > intA));
+console.log(strB + " == " + intA + " : " + (strB == intA));
+
 
 
--- test/variable/str_logic.js
+++ test/variable/str_logic.js
@@ -7,23 +7,28 @@ var strF = "5";
 
 print("TEST STRING == != < >");
 print("---------------------------");
-print(strA + " != " + strB + " = " + (strA!=strB));
-print(strB + " == " + strC + " = " + (strB==strC));
+print(strA + " != " + strB + " : " + (strA!=strB));
+print(strB + " == " + strC + " : " + (strB==strC));
 
-print(strA + " < " + strB + " = " + (strA<strB));
+print(strA + " < " + strB + " : " + (strA<strB));
 print("");
 
 print("COMPARE NUM STRING AND NUM STRING");
 print("---------------------------");
-print(strE + " < " + strF + " = " + (strE < strF));
+print(strE + " < " + strF + " : " + (strE < strF));
 print("");
 
-
+var intA = 5;
 print("COMPARE NUM STRING AND NUM");
 print("---------------------------");
-print(strA + " != " + strB + " = " + (strA!=strB));
-print(strB + " == " + strC + " = " + (strB==strC));
+print(strE + " < " + intA + " : " + (strE < intA));
+print("");
 
-print(strA + " < " + strB + " = " + (strA<strB));
+print("COMPARE STRING AND NUM");
+print("---------------------------");
+print(strB + " < " + intA + " : " + (strB < intA));
+print(strB + " > " + intA + " : " + (strB > intA));
+print(strB + " == " + intA + " : " + (strB == intA));
+
 
 
